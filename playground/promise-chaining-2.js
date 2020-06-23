46c0d67c3e79784c3fa998f0582c278ddd4ff0f7
Gurpreetjhamat11 <57304112+Gurpreetjhamat11@users.noreply.github.com>
2020-06-22 23:55:13 -0400

    night commit

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -2,11 +2,23 @@ require('../src/db/mongoose')
 // const User = require('./models/user')
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5edd82016f465831c044b3a5').then((task) => {
-    console.log(task);
-    return Task.countDocuments({completed: false})
-}).then((result) => {
-    console.log(result);
-}).catch((error) => {
-    console.log(error)
+// Task.findByIdAndDelete('5edd82016f465831c044b3a5').then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({completed: false})
+// }).then((result) => {
+//     console.log(result);
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({completed: false});
+    return task;
+}
+
+deleteTaskAndCount('5edd82016f465831c044b3a5').then((count) => {
+    console.log(count);
+}).catch((e) => {
+    console.log(error, e);
 })
