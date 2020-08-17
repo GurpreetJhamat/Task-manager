0ae8841fc9fea485eae30f36c102b908e2f09dec
Gurpreetjhamat11 <57304112+Gurpreetjhamat11@users.noreply.github.com>
2020-08-16 21:22:43 -0400

    middleware

--- src/router/user.js
+++ src/router/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
-
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -32,14 +32,17 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
     
-    try{
-        const user = await User.find({});
-        res.send(user)
-    } catch(e) {
-        res.status(500).send();
-    }
+    
+    
+    // try{
+    //     const user = await User.find({});
+    //     res.send(user)
+    // } catch(e) {
+    //     res.status(500).send();
+    // }
 
     // User.find({}).then((users) => {
     //     res.send(users)
